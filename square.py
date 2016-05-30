cab209004d68a1c337fbca20bebfc2cbac5acce7
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-05-29 21:51:18 -0300

    Alteração na forma de exibir os adjacentes.

--- square.py
+++ square.py
@@ -21,16 +21,14 @@ class Square(object):
 
     def __str__(self):
         """Retorna uma representação em string do quadrado"""
-        square_str = ' '
+        square_str = '[T'
         square_str += str(self.adjacents['top'].label)
-        square_str += ' \n'
-        square_str += str(self.adjacents['left'].label)
-        square_str += ' '
-        square_str += str(self.label)
-        square_str += ' '
-        square_str += str(self.adjacents['right'].label)
-        square_str += '\n '
+        square_str += ', B'
         square_str += str(self.adjacents['bottom'].label)
-        square_str += ' \n\n'
+        square_str += ', L'
+        square_str += str(self.adjacents['left'].label)
+        square_str += ', R'
+        square_str += str(self.adjacents['right'].label)
+        square_str += ']'
 
         return square_str
